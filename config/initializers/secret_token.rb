bd195ab06163c5a67c44c325576b72b1fa91fe75
Patrick Baselier <patrick@kabisa.nl>
2012-08-08 07:38:29 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterBrowser::Application.config.secret_token = '019c8bfa178e6647b40a2329cb63788a87a56dcee933bf41ab669dea6e70a5c544043f5b7d63da398875d1139c28a5853ac30bb586735f2869d3753b1e813999'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TwitterBrowser::Application.config.session_store :cookie_store, key: '_twitter_browser_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TwitterBrowser::Application.config.session_store :active_record_store
